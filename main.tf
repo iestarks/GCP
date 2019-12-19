18e763855206aa1d569ed17420073a7c252c7719
wolfpacker <ncsu.ee2017@gmail.com>
2019-12-19 14:13:07 -0500

    Terraform GCP 1st commit

---
+++ main.tf
@@ -0,0 +1,62 @@
+
+resource "random_id" "instance_id" {
+  byte_length = 8
+}
+
+resource "google_compute_instance" "default" {
+  name         = "vm-${random_id.instance_id.hex}"
+  project      =  "terraform-gcp-255501"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1904-disco-v20191217"
+    }
+  }
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Hello from Terraform on Google Cloud!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+  // Apply the firewall rule to allow external IPs to access this instance
+  tags = ["http-server"]
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http"
+  project      =  "terraform-gcp-255501"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "20"]
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+}
+
+resource "google_compute_network" "starksentvpc" {
+  name = "starksentvpc"
+  project      =  "terraform-gcp-255501"
+}
+
+output "ip" {
+  value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
+}
+
+
+
+
+
+
+
+
