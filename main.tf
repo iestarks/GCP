37254044a8383eda8632ca0391596d8065c966e7
wolfpacker <ncsu.ee2017@gmail.com>
2019-12-24 12:48:20 -0500

    Udeny vm files

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "google_compute_instance" "default" {
+  name         = "udemy-demo-instance"
+  machine_type = "${var.machine_type}"
+  zone         = "us-east1-b"
+
+  tags = ["name", "udemy-demo"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.image}"
+    }
+  }
+
+  // Local SSD disk
+  //scratch_disk {
+   // interface = "SCSI"
+  //}
+
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    name = "udemy_demo"
+  }
+
+  //metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+output "name" { value = "${google_compute_instance.default.name}"}
---
+++ variable.tf
@@ -0,0 +1,7 @@
+variable "image" { default = "ubuntu-1804-lts"}
+
+variable "machine_type" { default = "n1-standard-1"}
+
+
+
+
