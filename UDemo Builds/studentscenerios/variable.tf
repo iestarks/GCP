11e430a97416cb66a50b7026b94e686dd6cbc7b1
Irving <ncsu.ee2017@gmail.com>
2020-01-18 21:47:30 -0500

    adding scenarios

---
+++ UDemo Builds/studentscenerios/variable.tf
@@ -0,0 +1,18 @@
+variable "image" { default = "ubuntu-minimal-1804-lts"}
+
+#####got rid of it to allow more control of what type of machine is provisioned.
+# variable "machine_type" { 
+    #type = map
+   # default = {
+    #    dev = "n1-standard-1"
+     #   prod = "Production-wont-work"
+    # }
+
+variable "zone" { default = "us-east1-c"}
+variable "name_count" {default = ["udemy1", "udemy2", "udemy3"]}
+variable "machine_count" { default = "1"}
+variable "environment" { default = "production"}
+variable "machine_type" { default = "n1-standard-1"}
+variable "machine_type_dev" { default = "n1-standard-2"}
+
+
