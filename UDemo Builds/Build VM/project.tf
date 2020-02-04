f2083be3aa5f69f179d728281723957a216e02e2
Starks <Starks.Irving@mayo.edu>
2020-02-04 13:57:15 -0600

    changing provider to new project azuredevops

--- UDemo Builds/Build VM/project.tf
+++ UDemo Builds/Build VM/project.tf
@@ -1,7 +1,7 @@
 //variable = "path" {default = "/Users/irvs/terraform/googlecloud/secretkeys"}
 
 provider "google" {
- project = "terraform-gcp-255501"
+ project = "azuredevops-2020"
  region =  "us-east1-b"
  #credentials = file ("/Users/irvs/GCP/secret-255501.json")
 }
