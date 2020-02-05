2a9b443b2f757355c404d7cdcdfd3feaee708b51
Starks <Starks.Irving@mayo.edu>
2020-02-05 10:05:51 -0600

    adding load balancer

---
+++ UDemo Builds/Load balancer/project.tf
@@ -0,0 +1,11 @@
+
+
+provider "google-beta" {
+ project = "azuredevops-2020"
+ #region =  "us-east1-b"
+  region        = "us-central1"
+ #credentials = file ("/Users/irvs/terraform/googlecloud/GCP-Terraform-Resources/UDemo Builds/Build Instance/secretkeys/secret-255501.json")
+}
+
+
+
---
+++ UDemo Builds/Load balancer/variable.tf
@@ -0,0 +1,9 @@
+variable "bucket-name" { default = "udemy-test-bucket"}
+
+variable "region" { default = "us-central1"}
+
+variable "storage" { default = "REGIONAL"}
+
+variable "project" { default = "azuredevops-2020" }
+
+variable "provider-beta" { default = "google.beta" }
